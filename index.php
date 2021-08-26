4f8b08986d177011d1ab71348994405d5f5b3e44
Lazzytchik <GitLazzytchik@users.noreply.github.com>
2021-08-26 17:06:15 +0300

    1.01

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     //  FRONT CONTROLLER
 
     //  1. Общие настройки
-    ini_set('display_errors', 1);
+    ini_set('display_errors', 0);
     error_reporting(E_ALL);
 
     //  2. Подключение фалов системы
